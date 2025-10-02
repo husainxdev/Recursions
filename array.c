e7f471d3cd4f46753627c2f86c5ab73a29c51dd5
Hussain <humaidiofficil408@gmail.com>
2025-10-03 01:13:31 +0600

    added array

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int arry[10];
+printf("Enter your number : ");
+for (int i = 0; i <10 ; i++)
+
+{
+    scanf("%d",&arry[i]);
+}
+for (int i = 0; i <10 ; i++){
+    printf("array : %d\n ",arry[i]);
+
+}
+
+
+
+}
