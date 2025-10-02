c08509cde4d12abf859b3281a5a46da3bcc38b30
Hussain <humaidiofficil408@gmail.com>
2025-10-03 01:37:48 +0600

    added array

--- array.c
+++ array.c
@@ -2,6 +2,7 @@
 int main()
 {
 int arry[10];
+int sum=0;
 printf("Enter your number : ");
 for (int i = 0; i <10 ; i++)
 
@@ -9,10 +10,10 @@ for (int i = 0; i <10 ; i++)
     scanf("%d",&arry[i]);
 }
 for (int i = 0; i <10 ; i++){
-    printf("array : %d\n ",arry[i]);
+    sum+= arry[i];
 
 }
 
-
+printf("array : %d\n ",sum);
 
 }
